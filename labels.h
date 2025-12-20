ed9d5ede4e3521aa9d9ed913963da0d41568e374
sangderenard <47421372+sangderenard@users.noreply.github.com>
2025-12-20 08:03:35 -0600

    Fix tool menu build errors

---
+++ labels.h
@@ -0,0 +1,52 @@
+#pragma once
+
+// Canvas control bar labels
+#define LABEL_CANVAS_TOOL_SELECT "Select"
+#define LABEL_CANVAS_TOOL_NEW_TABLE "New Table"
+#define LABEL_CANVAS_TOOL_EDGE_MODE "Edge Mode"
+#define LABEL_CANVAS_TOOL_NEW_STAGE "New Stage"
+
+#define LABEL_TABLE_TOOL_SELECT "Tbl Select"
+#define LABEL_TABLE_TOOL_EDIT "Tbl Edit"
+#define LABEL_TABLE_TOOL_MORE "Tbl More"
+
+#define LABEL_CANVAS_TOOL_SHORT_SELECT "S"
+#define LABEL_CANVAS_TOOL_SHORT_TABLE "T"
+#define LABEL_CANVAS_TOOL_SHORT_EDGE "E"
+#define LABEL_CANVAS_TOOL_SHORT_STAGE "G"
+
+#define LABEL_TABLE_TOOL_SHORT_SELECT "s"
+#define LABEL_TABLE_TOOL_SHORT_EDIT "e"
+#define LABEL_TABLE_TOOL_SHORT_MORE "m"
+
+// Rope bar labels
+#define LABEL_ROPE_SEGS_DEC "Seg -"
+#define LABEL_ROPE_SEGS_INC "Seg +"
+#define LABEL_ROPE_SLACK_DEC "Slack -"
+#define LABEL_ROPE_SLACK_INC "Slack +"
+#define LABEL_ROPE_SEGS_PREFIX "segs:"
+#define LABEL_ROPE_SLACK_PREFIX " slack:"
+
+// IO row labels
+#define LABEL_IO_INPUT "INPUT"
+#define LABEL_IO_OUTPUT "OUTPUT"
+#define LABEL_IO_IN_SHORT "in"
+#define LABEL_IO_OUT_SHORT "out"
+#define LABEL_IO_MINUS "-"
+#define LABEL_IO_PLUS "+"
+
+// Menu labels
+#define LABEL_MENU_TITLE "Tool Menu"
+#define LABEL_MENU_STACK_TITLE "Stack"
+#define LABEL_MENU_KEY_RECORDER "Key Recorder"
+
+// Tool labels
+#define LABEL_TOOL_ADD "Add"
+#define LABEL_TOOL_SUB "Subtract"
+#define LABEL_TOOL_MUL "Multiply"
+#define LABEL_TOOL_DIV "Divide"
+#define LABEL_TOOL_MOD "Modulo"
+
+// Module labels
+#define LABEL_MODULE_TABLE "Table"
+#define LABEL_MODULE_STAGE "Stage"
